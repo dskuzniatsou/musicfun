94833dab6fca418d0fc55877cbc8872529c75b9b
dskuzniatsou <sovyclub@gmail.com>
2026-03-12 21:25:33 +0300

    Infinity Queries commit

--- src/features/tracks/ui/TracksPage.tsx
+++ src/features/tracks/ui/TracksPage.tsx
@@ -1,7 +1,27 @@
+
+
+import {useFetchTracksInfiniteQuery} from "@/features/tracks/api/tracksApi.ts";
+
+import {useInfiniteScroll} from "@/common/hooks";
+import {TracksList} from "@/features/tracks/ui/TracksList/TracksList.tsx";
+import {LoadingTrigger} from "@/features/tracks/ui/LoadingTrigger/LoadingTrigger.tsx";
+
 export const TracksPage = () => {
+    const { data, isFetching, isFetchingNextPage, fetchNextPage, hasNextPage } =
+        useFetchTracksInfiniteQuery()
+
+    const { observerRef } = useInfiniteScroll({ hasNextPage, isFetching, fetchNextPage })
+
+    const pages = data?.pages.flatMap(page => page.data) || []
+
     return (
         <div>
             <h1>Tracks page</h1>
+            <TracksList tracks={pages} />
+            {hasNextPage && (
+                <LoadingTrigger isFetchingNextPage={isFetchingNextPage} observerRef={observerRef} />
+            )}
+            {!hasNextPage && pages.length > 0 && <p>Nothing more to load</p>}
         </div>
     )
 }
